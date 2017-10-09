12b88d0930e5aaa97706dc0869cffb495c852ea5
Emmett Z. Budd <budd@joseph.cedarville.edu>
2017-10-08 22:30:21 -0400

    Basic improvements to views

--- symfony_project/src/AppBundle/Controller/SubmitController.php
+++ symfony_project/src/AppBundle/Controller/SubmitController.php
@@ -5,14 +5,13 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-// use Symfony\Component\HttpFoundation\Request;
 
 class SubmitController extends Controller
 {
  
-  public function submitAction($problem_id=1) {
+  public function submitAction($project_id=1) {
     return $this->render('courses/assignments/submit/index.html.twig', [
-			'problem_id' => $problem_id,
+			'project_id' => $project_id,
     ]);
   }
 }
