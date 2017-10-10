d975ff1806a163737802e038639c88a1440081cd
Emily R. Wolf <wolf@joseph.cedarville.edu>
2017-10-10 13:24:31 -0400

    Pushing for provider fixing

--- symfony_project/src/AppBundle/Controller/DefaultController.php
+++ symfony_project/src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\User;
 use AppBundle\Entity\Course;
 use AppBundle\Entity\UserSectionRole;
 
+use Auth0\SDK\Auth0;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,40 +16,11 @@ use Psr\Log\LoggerInterface;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="index")
-     */
     public function indexAction(LoggerInterface $logger) {
-
-		// $em = $this->getDoctrine()->getManager();
-		// $query = $em->createQuery('SELECT u FROM AppBundle\Entity\User u WHERE 1=1');
-		// $users = $query->getResult();
-    //
-		// $query = $em->createQuery('SELECT c FROM AppBundle\Entity\Course c WHERE 1=1');
-		// $courses = $query->getResult();
-    //
-		// $query = $em->createQuery('SELECT usr FROM AppBundle\Entity\UserSectionRole usr WHERE usr.section=22');
-		// $course = $query->getResult();
-    //
-    //     // replace this example code with whatever you need
-    //     return $this->render('default/index.html.twig', [
-    //         'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-		// 	'users' => $users,
-		// 	'courses' => $courses,
-		// 	'course' => $course,
-    //     ]);
-      $logger->info("CHRIS'S LG");
-      $logger->error(error);
       return $this->render('default/index.html.twig');
     }
 
-    /**
-    *
-    *
-    * @Route("/account", name="account")
-    */
-    public function accountAction(LoggerInterface $logger) {
-      $logger->error("This is the beginning of indexAction");
+    public function accountAction() {
       $em = $this->getDoctrine()->getManager();
   		$query = $em->createQuery('SELECT u FROM AppBundle\Entity\User u WHERE 1=1');
   		$users = $query->getResult();
@@ -56,24 +28,44 @@ class DefaultController extends Controller
   		$query = $em->createQuery('SELECT c FROM AppBundle\Entity\Course c WHERE 1=1');
   		$courses = $query->getResult();
 
-  		$query = $em->createQuery('SELECT usr FROM AppBundle\Entity\UserSectionRole usr WHERE usr.section=22');
-  		$course = $query->getResult();
-
-      $logger->info("CHRIS'S LOG!!!!!!");
-      $logger->info("USERS");
-      $logger->info(json_encode($users));
-      $logger->info("COURSES");
-      $logger->info (json_encode($courses));
-      $logger->info("COURSE");
-      $logger->info(json_encode($course));
-
           // replace this example code with whatever you need
           return $this->render('default/account/index.html.twig', [
               'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
   			'users' => $users,
   			'courses' => $courses,
-  			'course' => $course,
           ]);
       // return $this->render('default/account/index.html.twig');
     }
+	
+	public function addUserAction($userName){	
+
+	
+	$auth0 = new Auth0([
+ 	 'domain' => 'gradel.auth0.com',
+ 	 'client_id' => '2Ea1x0z0nSMqq3WWBs24pnzZFcqfA567',
+ 	 'client_secret' => 'T5ZhU-bvX5RlTAigtWQIyRe4x_nfvDIxV8FylaotMmI0RI-WLyRt4fgMXMG4wpEH',
+ 	 'redirect_uri' => 'http://joseph.cedarville.edu/gradel_dev/wolf/gradel/symfony_project/web/account',
+ 	 'audience' => 'https://gradel.auth0.com/userinfo',
+  	'persist_id_token' => true,
+ 	 'persist_access_token' => true,
+ 	 'persist_refresh_token' => true,
+	]);
+	$userInfo = $auth0->getUser();
+	if (!$userInfo) {
+	    echo json_encode("sad");
+	    // redirect to Login
+	} else {
+	    echo json_encode("He1y");
+	    // Say hello to $userInfo['name']
+	    // print logout button
+	}	
+		//$em = $this->getDoctrine()->getManager();
+		//$new_user = new User($userName, "Wolf", "", new \DateTime("now"), "");
+		//$em->persist($new_user);
+		//echo json_encode("He1y");
+		//echo json_encode($userName);
+		//return $this->render('default/account/index.html.twig');
+	}
+	
+
 }
