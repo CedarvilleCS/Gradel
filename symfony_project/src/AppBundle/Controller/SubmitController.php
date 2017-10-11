15415af83d671890282d0aa284e4ca6e2a628f50
Emmett Z. Budd <budd@joseph.cedarville.edu>
2017-10-11 15:38:34 -0400

    Added ugly file uploader that works

--- symfony_project/src/AppBundle/Controller/SubmitController.php
+++ symfony_project/src/AppBundle/Controller/SubmitController.php
@@ -10,6 +10,35 @@ class SubmitController extends Controller
 {
  
   public function submitAction($project_id=1) {
+
+    $uploadMessage = "";
+    $target_dir = "/var/www/gradel_dev/budd/"; // Specify an upload location
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "Overwriting existing file\n";
+        $uploadOk = 1; // Change to 0 if you don't want it to upload and overwrite. You can add a hash to the name or something.
+    }
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+
     return $this->render('courses/assignments/submit/index.html.twig', [
 			'project_id' => $project_id,
     ]);
