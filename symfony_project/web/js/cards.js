977b21555efc3a03eb734c974ff9efd2971b4aa2
Emmett Z. Budd <ebudd@cedarville.edu>
2017-11-05 21:39:50 -0500

    Made the arrows on cards rotate

--- symfony_project/web/js/cards.js
+++ symfony_project/web/js/cards.js
@@ -1,3 +1,9 @@
-function collapseCard(cardID) {   
+function collapseCard(cardID) {
+    active: true;
     $('#' + cardID).children().slideToggle();
+    if($('#' + cardID).parent().find("h2").find("span").find("i").hasClass("fa-rotate-90")) {
+        $('#' + cardID).parent().find("h2").find("span").find("i").removeClass("fa-rotate-90");
+    } else {
+        $('#' + cardID).parent().find("h2").find("span").find("i").addClass("fa-rotate-90");
+    }
 };
