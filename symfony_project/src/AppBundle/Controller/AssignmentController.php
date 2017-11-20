33b9e585a24811990f02ada0ee5ffb46d1dfa507
timo352 <timothyglensmith@cedarville.edu>
2017-11-20 00:42:32 -0500

    Created framework for javascript for assignment creation/editing

--- symfony_project/src/AppBundle/Controller/AssignmentController.php
+++ symfony_project/src/AppBundle/Controller/AssignmentController.php
@@ -16,6 +16,8 @@ use \DateTime;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Routing\Router;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
@@ -166,30 +168,23 @@ class AssignmentController extends Controller {
 	
 	
     public function modifyPostAction(Request $request) {
-		$em = $this->getDoctrine()->getManager();
-		$user = $this->get('security.token_storage')->getToken()->getUser();
-
-		$assignment = new Assignment();
-		$section = $em->find('AppBundle\Entity\Section', $sectionId);
-
-		$gradingmethod = $em->find('AppBundle\Entity\AssignmentGradingMethod', 1);
-
-		$assignment->name = $name;
-		$assignment->description = $description;
-		$assignment->section = $section;
-		$assignment->start_time = new DateTime("now");
-		$assignment->end_time = new DateTime("2050-01-01");
-		$assignment->cutoff_time = new DateTime("2050-01-01");
-		$assignment->weight = 0;
-		$assignment->is_extra_credit = false;
-		$assignment->gradingmethod = $gradingmethod;
-
-		$em->persist($assignment);
-		$em->flush();
-
-		return new RedirectResponse($this->generateUrl('assignment_edit', array('sectionId' => $sectionId, 'assignmentId' => $assignment->id)));
-
+		
+		return $this->returnForbiddenResponse("UHOH!");
+		
+		$url = "HELLO!";
+		
+		$response = new Response(json_encode(array('redirect_url' => $url)));
+		$response->headers->set('Content-Type', 'application/json');
+		$response->setStatusCode(Response::HTTP_OK);
+		
+		return $response;
     }
+	
+	private function returnForbiddenResponse($message){		
+		$response = new Response($message);
+		$response->setStatusCode(Response::HTTP_FORBIDDEN);
+		return $response;
+	}
 }
 
 ?>
