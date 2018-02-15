d4a6829f451f6318d02e54737d7c0548277ec936
Christopher D. Brauns II <brauns@joseph.cedarville.edu>
2018-02-15 13:22:55 -0500

    Added server scripts

--- symfony_project/server_start.sh
+++ symfony_project/server_start.sh
@@ -1,14 +1,3 @@
 ./update_cache
-php bin/console gos:websocket:server
-
-
-# if [ -z "$1" ]; then
-#   echo "Error! Must provide username for websocket starter";
-#   exit
-# fi
-# sudo echo -e "Checking for Currently Running Servers..."
-# pid=$(sudo ps aux | grep x'bin/console gos:websocket' | grep $1)
-# sudo echo -e "Server running with pid $pid"
-# ./update_cache
-# php bin/console gos:websocket:server &
-
+nohup php bin/console gos:websocket:server > socket.log 2>&1 &
+echo $! > save_pid.txt
