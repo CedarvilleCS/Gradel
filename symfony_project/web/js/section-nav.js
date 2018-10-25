e8244d456c178dcb8e84b2c5654299caa5d2a24e
Nathan Barlow <nathanbarlow@cedarville.edu>
2018-10-25 15:14:08 -0400

    Start changing sidenav into ajax

--- symfony_project/web/js/section-nav.js
+++ symfony_project/web/js/section-nav.js
@@ -12,7 +12,71 @@ function chevronClick(element, selection) {
         element.src = element.src.replace('white_chevron', 'white_expand_more');
     }
 }
-
-$.get(document.getElementById('SideNavPath').dataset.path, function (data) {
-    console.log(data);
+var viewData = document.getElementById('SideNavData').dataset;
+//This ajax callback function has a lot of wierd non-standard formatting, however this is so that you can read the HTML it generates more easily
+$.get(viewData.path, function (data) {
+    var html = '';
+    if (data.usersectionroles.length > 0) {
+        //List all of the sections being taken
+        data.sections_taking.forEach((section, sectionIndex) => {
+            html += `<div class="section-nav-container">
+			            <div>
+                            <div class="nav-chevron-title-container">`;
+            if (section.assignments.length > 0) {
+                html +=         `<img class="nav-chevron" onclick="chevronClick(this, 'section-${section.id}')" src="{{asset('images/white_chevron.png')}}" />`;
+            }
+            html +=             `<a href="{{path('section', {'sectionId':section.id})}}">${section.course.code}</a>
+                            </div>`;
+            section.assignments.forEach((assignment, assignmentIndex) => {
+                html +=     `<div class="nav-assignment section-${section.id}">
+                                <div class="nav-chevron-title-container">`;
+                if (assignment.problems.length > 0) {
+                    html +=         `<img class="nav-chevron" onclick="chevronClick(this, 'assignment-${assignment.id}')" src="{{asset('images/white_chevron.png')}}" />`;
+                } else {
+                    //Empty div here for css-grid to align things correctly
+                    html +=         `<div></div>`;
+                }
+                html +=             `<a href="{{path('assignment', {'sectionId':assignment.section.id, 'assignmentId':assignment.id})}}">${assignment.name}</a>
+                                </div>`;
+                assignment.problems.forEach((problem, problemIndex) => {
+                    html +=     `<div class="nav-problem assignment-${assignment.id}"><a href="{{path('assignment', {'sectionId':assignment.section.id, 'assignmentId':assignment.id, 'problemId':problem.id})}}">${problem.name}</a></div>`;
+                });
+                html +=     `</div>`;
+            });
+            html +=     `</div>
+                    </div>`;
+        });
+    }
+    if (data.sections_teaching.length > 0) {
+        //List all of the sections being taught
+        data.sections_teaching.forEach((section, sectionIndex) => {
+            html += `<div class="section-nav-container">
+			            <div>
+                            <div class="nav-chevron-title-container">`;
+                if (section.assignments.length > 0) {
+                    html +=     `<img class="nav-chevron" onclick="chevronClick(this, 'section-${section.id}')" src="{{asset('images/white_chevron.png')}}" />`;
+                }
+            html +=             `<a href="{{path('section', {'sectionId':section.id})}}">${section.course.code}</a>
+                            </div>`;
+            section.assignments.forEach((assignment, assignmentIndex) => {
+                html +=     `<div class="nav-assignment section-${section.id}">
+                                <div class="nav-chevron-title-container">`;
+                if (assignment.problems.length > 0) {
+                    html +=         `<img class="nav-chevron" onclick="chevronClick(this, 'assignment-${assignment.id}')" src="{{asset('images/white_chevron.png')}}" />`;
+                } else {
+                    //Empty div here for css-grid to align things correctly
+                    html +=         `<div></div>`;
+                }
+                html +=             `<a href="{{path('assignment', {'sectionId':assignment.section.id, 'assignmentId':assignment.id})}}">${assignment.name}</a>
+                                </div>`;
+                assignment.problems.forEach((problem, problemIndex) => {
+                    html +=     `<div class="nav-problem assignment-${assignment.id}"><a href="{{path('assignment', {'sectionId':assignment.section.id, 'assignmentId':assignment.id, 'problemId':problem.id})}}">${problem.name}</a></div>`;
+                });
+                html +=     `</div>`;
+            });
+            html +=     `</div>			
+                    </div>`;
+        });
+    }
+    document.getElementById('SideNavContent').innerHTML = html;
 });
