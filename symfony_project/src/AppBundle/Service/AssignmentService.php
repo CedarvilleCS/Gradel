5f33d4f4360f93e39688555c56715da718ce0159
JamesOsborne <james.osborne112@gmail.com>
2018-11-01 16:03:40 -0400

    Alphabetize AssignmentService methods

--- symfony_project/src/AppBundle/Service/AssignmentService.php
+++ symfony_project/src/AppBundle/Service/AssignmentService.php
@@ -22,11 +22,6 @@ class AssignmentService
 		return $assignment;
 	}
 
-	public function insertAssignment($entityManager, $assignment) {
-		$entityManager->persist($assignment);
-		$entityManager->flush();
-	}
-
 	public function deleteAssignment($entityManager, $assignment) {
 		$entityManager->remove($assignment);
 		$entityManager->flush();
@@ -35,22 +30,22 @@ class AssignmentService
 	public function getAssignmentById($entityManager, $assignmentId) {
 		return $entityManager->find("AppBundle\Entity\Assignment", $assignmentId);
 	}
-
+	
     public function getAssignmentsSortedByDueDateForHome($entityManager, $sections) {
-        $twoWeeksDate = new DateTime();
+		$twoWeeksDate = new DateTime();
 		$twoWeeksDate = $twoWeeksDate->add(new DateInterval("P2W"));
 		
 		$builder = $entityManager->createQueryBuilder();
 		$builder->select("a")
-			->from("AppBundle\Entity\Assignment", "a")
-			->where("a.section IN (?1)")
-			->andWhere("a.end_time > (?2)")
-			->andWhere("a.end_time < (?3)")
-			->setParameter(1, $sections)
-			->setParameter(2, new DateTime())
-			->setParameter(3, $twoWeeksDate)
-			->orderBy("a.end_time", "ASC");
-			
+		->from("AppBundle\Entity\Assignment", "a")
+		->where("a.section IN (?1)")
+		->andWhere("a.end_time > (?2)")
+		->andWhere("a.end_time < (?3)")
+		->setParameter(1, $sections)
+		->setParameter(2, new DateTime())
+		->setParameter(3, $twoWeeksDate)
+		->orderBy("a.end_time", "ASC");
+		
 		$assignmentQuery = $builder->getQuery();		
 		return $assignmentQuery->getResult();
 	}
@@ -58,12 +53,17 @@ class AssignmentService
 	public function getAssignmentsBySection($entityManager, $section) {
 		$builder = $entityManager->createQueryBuilder();
 		$builder->select("a")
-			->from("AppBundle\Entity\Assignment", "a")
-			->where("a.section = (?1)")
-			->setParameter(1, $section);
-
+		->from("AppBundle\Entity\Assignment", "a")
+		->where("a.section = (?1)")
+		->setParameter(1, $section);
+		
 		$assignmentQuery = $builder->getQuery();
 		return $assignmentQuery->getResult();
+	}
+
+	public function insertAssignment($entityManager, $assignment) {
+		$entityManager->persist($assignment);
+		$entityManager->flush();
 	}
 }
 ?>
