9952538b13b8137979a39749a61ad7a2dd80a05f
Nathan Barlow <nathanbarlow@cedarville.edu>
2018-12-12 14:10:14 -0500

    Remove floating-control.js and replace it with spacer divs in every page that has floating controls as well as fix the users header messing up on small widths

--- symfony_project/web/js/floating-control.js
+++
@@ -1,5 +0,0 @@
-//Set main_content min-height to the height of the content plus 100px to make sure
-//that the floating controls will never cover something with no way to see it.
-let mainContent = document.querySelector('.main_content');
-let contentHeight = mainContent.clientHeight;
-mainContent.style.minHeight = `${contentHeight + 100}px`;
