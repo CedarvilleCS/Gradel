1b10631b612ec3683d6a3a03e3e5f93d74197d23
JamesOsborne <james.osborne112@gmail.com>
2019-02-07 15:43:17 -0500

    Add missing userService->

--- symfony_project/src/AppBundle/Controller/ContestPostController.php
+++ symfony_project/src/AppBundle/Controller/ContestPostController.php
@@ -784,7 +784,7 @@ class ContestPostController extends Controller {
     }
     
     public function postQuestionAction(Request $request) {
-        $user = $this->getCurrentUser();
+        $user = $this->userService->getCurrentUser();
         if (!get_class($user)) {
             $this->returnForbiddenResponse("USER DOES NOT EXIST");
         }
